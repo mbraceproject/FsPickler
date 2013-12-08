1550c3f185a364256e170c8d8298bd143d5a5ec0
eiriktsarpalis <eirik.tsarpalis@gmail.com>
2013-12-08 15:55:28 +0200

    clear pre-build script

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using FsPickler;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = new Tuple<int, string>(42, "poutses mple");
+            var fsp = new FsPickler.FsPickler();
+
+            var p = fsp.GeneratePickler<Tuple<string, int>> ();
+
+            var bytes = fsp.Pickle(x);
+            var y = fsp.UnPickle<Tuple<int, string>>(bytes);
+
+            Console.WriteLine(x.Equals(y));
+
+            Console.ReadLine();
+        }
+    }
+}
