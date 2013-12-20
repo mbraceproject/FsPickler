69e3df0a30bca52629e78daca1618ab0838ada14
eiriktsarpalis <eirik.tsarpalis@gmail.com>
2013-12-20 23:58:54 +0200

    clean up fsx

--- FsPickler/Test.fsx
+++ FsPickler/Test.fsx
@@ -8,14 +8,6 @@ let fnv = FsPickler.Hashing.FNV1aStreamFactory()
 
 let value = [1..100000] |> List.map (fun i -> Some (string i, Some i))
 
-
-let xs = fsp.Pickle([1..10000])
-
-System.IO.File.WriteAllBytes("/mbrace/foo.txt", xs)
-let bytes = System.IO.File.ReadAllBytes("/mbrace/foo.txt")
-
-let xs' = fsp.UnPickle<int list> bytes
-
 #time
 
 for i = 0 to 100 do
