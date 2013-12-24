c63b3b883fde5704829e2fcb02a5e93a9b7edfdf
eiriktsarpalis <eirik.tsarpalis@gmail.com>
2013-12-24 19:09:08 +0200

    version number update

--- FsPickler/assemblyInfo.fs
+++ FsPickler/assemblyInfo.fs
@@ -5,8 +5,7 @@
 #if BUILD_STRONG_NAME
     [<assembly:AssemblyKeyFile("../../Lib/key.snk")>]
 #endif
-//    [<assembly:AssemblyKeyFile("../../Lib/key.snk")>]
-    [<assembly:AssemblyVersion("0.8.1.*")>]
+    [<assembly:AssemblyVersion("0.8.2.*")>]
     do()
 
 
