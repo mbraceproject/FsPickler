b1087c46bf19a78371472b96305ebff91e5e9b99
eiriktsarpalis <eirik.tsarpalis@gmail.com>
2014-07-16 19:43:08 +0300

    docs

--- docs/content/index.fsx
+++ docs/content/index.fsx
@@ -4,63 +4,76 @@
 #I "../../bin"
 
 (**
-FsPickler
-===================
+
+# FsPickler : A serializer for .NET
+
+FsPickler is a general-purpose serialization library that focuses on completeness and performance. 
+It supports multiple, pluggable serialization formats including Xml, JSON and BSON.
+The library is based on the functional programming concept of 
+[pickler combinators](http://lambda-the-ultimate.org/node/2243)
+which has been adapted to accommodate the object oriented nature of the .NET framework.
 
 <div class="row">
   <div class="span1"></div>
   <div class="span6">
     <div class="well well-small" id="nuget">
       The FsPickler library can be <a href="https://nuget.org/packages/FsPickler">installed from NuGet</a>:
-      <pre>PM> Install-Package FsPickler</pre>
+      <pre>PM> Install-Package FsPickler
+PM> Install-Package FsPickler.Json
+PM> Install-Package FsPickler.CSharp</pre>
     </div>
   </div>
   <div class="span1"></div>
 </div>
 
-Example
--------
+## Example
 
-This example demonstrates using a function defined in this sample library.
+This example demonstrates a basic serialization roundtrip using the library
 
 *)
 #r "FsPickler.dll"
 open Nessos.FsPickler
 
-let fsp = FsPickler.CreateBinary()
-let pickle = fsp.Pickle [Some 1; None ; Some -1]
-fsp.UnPickle<int option list> pickle
+let binary = FsPickler.CreateBinary()
+
+let pickle = binary.Pickle [Some 1; None ; Some -1]
+binary.UnPickle<int option list> pickle
 
 (**
 
-Samples & documentation
------------------------
+## Why FsPickler?
 
-The library comes with comprehensible documentation. 
-It can include a tutorials automatically generated from `*.fsx` files in [the content folder][content]. 
-The API reference is automatically generated from Markdown comments in the library implementation.
+The principal motivation behind creating FsPickler is the need for a
+library that offers efficient, correct and complete support for serializable
+objects in the .NET framework. It is aimed at providing a foundation for
+communication of arbitrary objects between .NET processes.
 
- * [Tutorial](tutorial.html) contains a further explanation of this sample library.
+FsPickler is ideally suited for serializing:
 
- * [API Reference](reference/index.html) contains automatically generated documentation for all types, modules
-   and functions in the library. This includes additional brief samples on using most of the
-   functions.
+ * Large and complex objects, such as dictionaries, trees and cyclic objects.
+ * Abstract classes, inherited classes, delegates and closures.
+ * ISerializable, IDeserializationCallback or attribute-based serializable types.
+ * F# unions, records and quotations.
+ * Inaccessible types or types unknown at compile time.
+
+FsPickler is NOT:
+
+ * a general-purpose XML/JSON/BSON framework.
+ * designed for cross-platform communication.
+
+## Documentation & Technical Overview
+
+Coming soon.
  
-Contributing and copyright
---------------------------
+## Contributing and copyright
 
 The project is hosted on [GitHub][gh] where you can [report issues][issues], fork 
-the project and submit pull requests. If you're adding new public API, please also 
-consider adding [samples][content] that can be turned into a documentation. You might
-also want to read [library design notes][readme] to understand how it works.
+the project and submit pull requests.
 
-The library is available under Public Domain license, which allows modification and 
-redistribution for both commercial and non-commercial purposes. For more information see the 
-[License file][license] in the GitHub repository. 
+The library is available under the MIT License. 
+For more information see the [License file][license] in the GitHub repository. 
 
-  [content]: https://github.com/fsprojects/FSharp.ProjectScaffold/tree/master/docs/content
-  [gh]: https://github.com/fsprojects/FSharp.ProjectScaffold
-  [issues]: https://github.com/fsprojects/FSharp.ProjectScaffold/issues
-  [readme]: https://github.com/fsprojects/FSharp.ProjectScaffold/blob/master/README.md
-  [license]: https://github.com/fsprojects/FSharp.ProjectScaffold/blob/master/LICENSE.txt
+  [gh]: https://github.com/nessos/FsPickler
+  [issues]: https://github.com/nessos/FsPickler/issues
+  [license]: https://github.com/nessos/FsPickler/blob/master/License.md
 *)
