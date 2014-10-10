c23e14b58965df6f823143139d5c3c094130f32a
eiriktsarpalis <eirik.tsarpalis@gmail.com>
2014-10-10 15:28:24 +0300

    version update

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,3 +1,6 @@
+### 1.0.0
+* Add support for IgnoreDataMemberAttribute.
+
 ### 0.9.11
 * Bug fixes.
 
--- src/FsPickler.Json/AssemblyInfo.fs
+++ src/FsPickler.Json/AssemblyInfo.fs
@@ -1,9 +1,9 @@
 ﻿namespace System
 open System.Reflection
 
-[<assembly: AssemblyVersionAttribute("0.9.11")>]
-[<assembly: AssemblyFileVersionAttribute("0.9.11")>]
+[<assembly: AssemblyVersionAttribute("1.0.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.9.11"
+    let [<Literal>] Version = "1.0.0"
