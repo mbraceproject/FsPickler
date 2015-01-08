9c76ce9ac83f4c4d4352d42b68b923fcb68435f1
Eirik Tsarpalis <eirik@nessos.gr>
2015-01-08 14:49:03 +0200

    cleanup

--- src/FsPickler/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [ ! -f packages/FAKE/tools/Fake.exe ]; then
-  mono .nuget/NuGet.exe install FAKE -OutputDirectory packages -ExcludeVersion
-fi
-if [ ! -f packages/SourceLink.Fake/tools/SourceLink.fsx ]; then
-  mono .nuget/NuGet.exe install SourceLink.Fake -OutputDirectory packages -ExcludeVersion
-fi
-mono packages/FAKE/tools/FAKE.exe build.fsx -d:MONO $@
