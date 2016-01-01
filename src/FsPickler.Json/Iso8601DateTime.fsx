38c579e1f23022b769b60c3bfd1c1a5d630a8bad
Phil de Joux <phil.dejoux@blockscope.com>
2016-01-02 03:32:10 +1300

    Add ISO 8601 test script, #68.

---
+++ src/FsPickler.Json/Iso8601DateTime.fsx
@@ -0,0 +1,26 @@
+﻿#I "../../bin/"
+#r "FsPickler.dll"
+#r "FsPickler.Json.dll"
+
+fsi.PrintLength <- 1000
+fsi.PrintWidth <- 1000
+
+open System
+open Nessos.FsPickler.Json
+
+let json = FsPickler.CreateJsonSerializer(indent = true)
+
+type GenericRecord<'T> = { GValue : 'T }
+type GenericRecordAsString = GenericRecord<string>
+
+let a0 = json.PickleToString<string>("a")
+let a0' = json.UnPickleOfString<string>(a0)
+
+let a1 = json.PickleToString<GenericRecordAsString>({ GValue = "a" })
+let a1' = json.UnPickleOfString<GenericRecordAsString>(a1)
+
+let b0 = json.PickleToString<string>("2013-10-30T16:00:00")
+let b0' = json.UnPickleOfString<string>(b0)
+
+let b1 = json.PickleToString<GenericRecordAsString>({ GValue = "2013-10-30T16:00:00" })
+let b1' = json.UnPickleOfString<GenericRecordAsString>(b1)
