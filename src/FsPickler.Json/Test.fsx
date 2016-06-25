639572724d5f35add95eb4b15bf5974eb4deef16
Andreas Vilinski <vilinski@yahoo.de>
2016-06-26 00:46:12 +0200

    update fork

--- src/FsPickler.Json/Test.fsx
+++ src/FsPickler.Json/Test.fsx
@@ -7,7 +7,8 @@ open Nessos.FsPickler.Json
 open Nessos.FsPickler.Combinators
 
 let jsp = FsPickler.CreateJsonSerializer(indent = true, omitHeader = true)
-let bsp = FsPickler.CreateBsonSerializer()
+let bsp = FsPickler.CreateBsonSerializer(omitHeader = true)
+
 
 type Record = { Name : string ; Age : int }
 
@@ -19,3 +20,10 @@ jsp.PickleToString [A 42 ; B("test", 0) ; C]
 
 let pickle = jsp.PickleToString <@ 1 + 1 @>
 jsp.UnPickleOfString<Quotations.Expr<int>> pickle
+let buffer = bsp.Pickle { Name = "me" ; Age = 12 }
+buffer |> Array.map (fun b -> char b)
+
+let len1 = System.BitConverter.ToInt32(buffer, 0)
+buffer |> Array.length
+
+System.Text.Encoding.UTF8.GetBytes "me"
