ed99bf49cee5cff8acd3b50a29306bbf27968a6b
Eirik Tsarpalis <eirik.tsarpalis@jet.com>
2018-03-19 08:05:15 +0000

    add ISerializable benchmark

--- tests/FsPickler.Benchmarks/Main.fs
+++ tests/FsPickler.Benchmarks/Main.fs
@@ -5,5 +5,6 @@ open BenchmarkDotNet.Running
 [<EntryPoint>]
 let main _ =
     //let _summary = BenchmarkRunner.Run<LargeObject.Roundtrip>()
-    let _summary = BenchmarkRunner.Run<Poco.Roundtrip>()
+    //let _summary = BenchmarkRunner.Run<Poco.Roundtrip>()
+    let _summary = BenchmarkRunner.Run<ISerializable.Roundtrip>()
     0
--- tests/FsPickler.Benchmarks/Types.fs
+++ tests/FsPickler.Benchmarks/Types.fs
@@ -4,13 +4,13 @@ open FsCheck
 
 module Poco =
     [<CLIMutable>]
-    type Foo =
+    type T =
         { A : int ; B : string ; C : bool ; D : byte[] }
 
     let value = { A = 42 ; B = "lorem ipsum" ; C = true ; D = [|1uy .. 20uy|] }
 
     type Roundtrip() =
-        inherit RoundtripBenchmark<Foo>(value)
+        inherit RoundtripBenchmark<T>(value)
 
 module LargeObject =
     type Foo = { A : int ; B : string ; C : bool }
@@ -22,3 +22,22 @@ module LargeObject =
     
     type Roundtrip() =
         inherit RoundtripBenchmark<T []>(value)
+
+module ISerializable =
+    open System.Runtime.Serialization
+
+    type T(x : int, y : string, z : bool) =
+        interface ISerializable with
+            member __.GetObjectData(si, _) =
+                si.AddValue("x", x)
+                si.AddValue("y", y)
+                si.AddValue("z", z)
+
+        new (si : SerializationInfo, _ : StreamingContext) =
+            let inline get x = si.GetValue(x, typeof<'T>) :?> 'T
+            new T(get "x", get "y", get "z")
+
+    let instance = new T(42, "lorem ipsum", true)
+
+    type Roundtrip() =
+        inherit RoundtripBenchmark<T>(instance)
