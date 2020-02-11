b9bd506f6550f6cfa99e38bb15b92b40a99f092e
Eirik Tsarpalis <eirik.tsarpalis@gmail.com>
2020-02-11 02:09:37 +0000

    disable paket restore for fake cli

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+cd `dirname $0`
+
 dotnet tool restore
+
+# Use paket and not fake for restoring packages
+# c.f. https://github.com/fsharp/FAKE/issues/2181
 dotnet paket restore
+export PAKET_SKIP_RESTORE_TARGETS=true
+
 dotnet fake run build.fsx "$@"
